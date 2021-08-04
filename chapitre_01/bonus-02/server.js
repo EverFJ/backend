b36288f96c0a66a6765e629be64ed01eee188282
Francois Janson <jansonfrancois@gmail.com>
2021-08-04 15:55:28 +0200

    cleaning code

--- chapitre_01/bonus-02/server.js
+++ chapitre_01/bonus-02/server.js
@@ -5,9 +5,9 @@ const app = express();
 const port = 8000;
 
 app.get("/:id", (req, res) => {
-    const pokemonId = req.params.id;
+    const offset = req.params.id - 1;
     const limit = 1;
-    request.get("https://pokeapi.co/api/v2/pokemon?limit=" + limit + "&offset=" + pokemonId, (err, result, body) => {
+    request.get("https://pokeapi.co/api/v2/pokemon?limit=" + limit + "&offset=" + offset, (err, result, body) => {
         const response = JSON.parse(result.body);
         const pokemon = response.results[0].name;
         // console.log(pokemon)
