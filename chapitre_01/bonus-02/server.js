6b7a4d4c6f699e5f004c0f13cd1493293f168c55
Francois Janson <jansonfrancois@gmail.com>
2021-08-04 15:30:28 +0200

    last bonus exercise

---
+++ chapitre_01/bonus-02/server.js
@@ -0,0 +1,19 @@
+const request = require("request");
+const express = require("express");
+const app = express();
+
+const port = 8000;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
+
+app.get("/:id", (req, res) => {
+    const pokemonId = req.params.id;
+    const limit = 1;
+    request.get("https://pokeapi.co/api/v2/pokemon?limit=" + limit + "&offset=" + pokemonId, (err, result, body) => {
+        const response = JSON.parse(result.body);
+        const pokemon = response.results[0].name;
+        // console.log(pokemon)
+        res.send(pokemon);
+    })
+})
